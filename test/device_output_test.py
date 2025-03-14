672b901eb219c70e07a6bd7234f13ef6c12be1e5
Elia Rizzo <eliarizzo@hotmail.it>
2025-03-14 19:41:48 +0100

    updating to last changings

--- test/device_output_test.py
+++ test/device_output_test.py
@@ -1,5 +1,6 @@
 from src.device_stream import output_stream
 from src.data_preprocessing import preprocess_and_split_data
+import time
 
 df_path = "src/data/iot_telemetry_data.csv"
 dfs_dict = preprocess_and_split_data(
@@ -11,4 +12,8 @@ dfs_dict = preprocess_and_split_data(
 df0 = dfs_dict["df0"]
 
 device_output = output_stream(df0)
+
+for _ in range(10):
+    print(next(device_output))
+    time.sleep(1)
 
