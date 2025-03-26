a57a823f3a5eacfed27c3b841893e798a8a7b72f
Elia Rizzo <eliarizzo@hotmail.it>
2025-03-26 10:09:47 +0100

    the lists are now queues. Added docstring to main.py

--- main.py
+++ main.py
@@ -13,17 +13,31 @@ import time
 from typing import Generator
 
 
-async def stream_data(temp_list: list, dates_list: list, device_output: Generator):
+async def stream_data(temp_list: list, dates_list: list, device_output: Generator) -> None:
+    """Simulate streaming data from a device, checks temperature warnings, and, whenever warnings are created, analize them with an AI agent.
+
+    Args:
+        temp_list (list): A list to store temperature data from the device output.
+        dates_list (list): A list to store the corresponding timestamps of the temperature readings, created on the spot.
+        device_output (Generator): A generator that yields device output, from which we get the temperature data. 
+
+    Returns:
+        None
+    """
     warnings_log = []
     warnings_log_path = "warnings_log.json"
+
     while True:
         await asyncio.sleep(0.01)
+
+        # generate the data and store them
         current_output = next(device_output)
         temp_list.append(current_output["temp"])
         dates_list.append(datetime.now().isoformat())
 
         warning = check_temperature_warning(current_output, 17., 19.8)
-
+        
+        # Start the ai agent in another thread whenever there are new warnings
         if warning:
             warnings_log.append(warning)
 
@@ -32,8 +46,18 @@ async def stream_data(temp_list: list, dates_list: list, device_output: Generato
             
             asyncio.create_task(asyncio.to_thread(write_agent_warnings))
 
-async def main():
+        # This makes the lists two queues
+        if len(temp_list) > 50:
+            temp_list.pop(0)
+            dates_list.pop(0)
 
+async def main():
+    """
+    Asynchrounously run the function 'stream_data', that stream and check the data, and 'plot_temp', that plot the temperature datas collected. 
+
+    Returns:
+        None
+    """
     data_path = "src/data/iot_telemetry_data.csv"
     dfs_dict = preprocess_and_split_data(data_path, "device", "ts", "ts")
     df = dfs_dict["df1"]
